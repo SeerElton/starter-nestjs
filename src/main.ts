259c4d51b246a5964c099f58e4c9f1bbfbeb424f
Elton Nhlana <nhlana.2@gmail.com>
2023-11-12 23:24:13 +0200

    deploy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { DataValidationInterceptor } from './interceptor/validation.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.enableCors();
+  app.enableCors({ allowedHeaders: '*', origin: '*', methods: '*' });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
